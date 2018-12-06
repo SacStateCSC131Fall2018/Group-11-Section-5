ab030e7431670fed8b6aa1eb1788e3de372a2bc7
akaid <akaid@10.117.115.92>
2018-12-05 16:01:06 -0800

    Rectified the group and section name in the dialog box for About

--- Sprint 1 - GUI/src/pirexProgram/AboutDialog.java
+++ Sprint 1 - GUI/src/pirexProgram/AboutDialog.java
@@ -30,7 +30,7 @@ import javax.swing.JMenuItem;
 		    name.setAlignmentX(0.5f);
 		    add(name);
 		    
-		    JLabel name0 = new JLabel("Section 5, Group 11");
+		    JLabel name0 = new JLabel("Group 11, Section 5");
 		    name.setAlignmentX(0.5f);
 		    add(name0);
 
